7b2b94fdb549eb131ed2db9250b90982a7e97e37
Görkem Altay <78072511+gorkemaltay1@users.noreply.github.com>
2023-01-14 10:32:54 +0300

    Time: 34 ms (72.84%), Space: 13.7 MB (99.81%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,3 +1,3 @@
 class Solution:
     def isStrictlyPalindromic(self, n: int) -> bool:
-        return False #Due to base (n-2), it returns always false. Because base(n-2) is always palindrmic.
+        return False #Due to base (n-2), it returns always false. Because base(n-2) is always not palindromic.
