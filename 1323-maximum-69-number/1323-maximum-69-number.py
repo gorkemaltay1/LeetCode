3f7882906712eda8bfc887a43c9be99f64e37326
Görkem Altay <78072511+gorkemaltay1@users.noreply.github.com>
2023-01-18 21:56:03 +0300

    Time: 39 ms (50.15%), Space: 13.8 MB (50.51%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -1,9 +1,11 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        numLen = len(str(num))
-        for i in range(numLen+1):
-            if str(num//10**(numLen-1-i))[-1] == "6":
-                num += 3*10**(numLen-1-i)
-                return num
-        return num
+        strNum = str(num)
+        try:
+            strNum = strNum[:strNum.index("6")] + "9" + strNum[strNum.index("6") + 1:] 
+            return int(strNum)
+        except:
+            return num
+       
+
             
