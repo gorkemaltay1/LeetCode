b0221df144840e37aec77f66433740220d8bf889
Görkem Altay <78072511+gorkemaltay1@users.noreply.github.com>
2023-01-18 12:02:16 +0300

    Time: 234 ms (97.63%), Space: 20.2 MB (82.27%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
@@ -1,14 +1,9 @@
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
-        if ruleKey == "type":
-            ruleKey = 0
-        if ruleKey == "color":
-            ruleKey = 1
-        if ruleKey == "name":
-            ruleKey = 2
+        d = {"type":0,"color":1,"name":2}
             
         count = 0
         for i in items:
-            if i[ruleKey]==ruleValue:
+            if i[d[ruleKey]]==ruleValue:
                 count += 1
         return count
