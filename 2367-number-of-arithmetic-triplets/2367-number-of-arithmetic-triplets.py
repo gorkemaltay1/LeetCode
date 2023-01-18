85332e4b4c79c55df82cd9d2204b1bbedc87d510
Görkem Altay <78072511+gorkemaltay1@users.noreply.github.com>
2023-01-18 13:03:00 +0300

    Time: 2784 ms (11.69%), Space: 108.3 MB (15.00%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -1,9 +1,10 @@
+from itertools import combinations
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        groupedList = list(combinations(nums,3))
         count = 0
-        for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                for k in range(j,len(nums)):
-                    if nums[k]-nums[j] == diff and nums[j] - nums[i] == diff:
-                        count += 1
+        
+        for i in groupedList:
+            if i[2] - i[1] == diff and i[1]-i[0] == diff:
+                count += 1
         return count        
