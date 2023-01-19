c1ea321fefb9fb239767c77a79804603f5ed44b6
Görkem Altay <78072511+gorkemaltay1@users.noreply.github.com>
2023-01-20 01:24:27 +0300

    Time: 56 ms (81.57%), Space: 13.9 MB (16.34%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,7 +1,6 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        nList = [i for i in range(1,n+1)]
-        for i in range(n//2,n):
-            if sum(nList[:i+1]) == sum(nList[i:]):
-                return i+1
+        for i in range(n//2,n+1):
+            if (i*(i+1))//2 == (n*(n+1))//2-((i-1)*i)//2:
+                return i
         return -1
