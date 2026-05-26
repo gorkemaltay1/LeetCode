91143d195b6a1e6f9bd15fc20cc3ddab01412373
Görkem Altay <78072511+gorkemaltay1@users.noreply.github.com>
2026-05-26 20:47:44 +0300

    Time: 23 ms (6.04%), Space: 19.4 MB (6.33%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,47 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        four = "IV"
+        nine = "IX"
+        forty = 'XL'
+        ninety = 'XC'
+        fourhundred = 'CD'
+        ninehundred = 'CM'
+        result = ""
+
+        print("binlik:",num // 1000)
+        print("yüzlük:",num // 100," ",num % 100," ",num // 100 % 10)
+        print("onluk:",num // 10," ",num % 10," ",num // 10 % 10)
+        print("birlik:",num // 1," ",num % 1," ",num % 10)
+
+        if num // 1000 > 0:
+            result += (num // 1000) * 'M'
+        if num // 100 % 10 > 0:
+            if num // 100 % 10 == 4:
+                result += fourhundred
+            elif num // 100 % 10 == 9:
+                result += ninehundred
+            elif num // 100 % 10 >= 0 and num // 100 % 10 < 5:
+                result += (num // 100 % 10) * "C"
+            elif num // 100 % 10 >= 5:
+                result += 'D' + ((num // 100 % 10) % 5) * 'C'
+        if num // 10 % 10 > 0:
+            if num // 10 % 10 == 4:
+                result += forty
+            elif num // 10 % 10 == 9:
+                result += ninety
+            elif num // 10 % 10 >= 0 and num // 10 % 10 < 5:
+                result += (num // 10 % 10) * "X"
+            elif num // 10 % 10 >= 5:
+                result += 'L' + ((num // 10 % 10) % 5) * 'X'
+        if num % 10 > 0:
+            if num % 10 == 4:
+                result += four
+            elif num % 10 == 9:
+                result += nine
+            elif num % 10 >= 0 and num % 10 < 5:
+                result += (num % 10) * "I"
+            elif num % 10 >= 5:
+                result += 'V' + ((num % 10) % 5) * 'I'
+
+
+        return result
